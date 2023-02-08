563eee497a24031778593f542c48b152ef66dc33
Amey Bawane <60310813+ameybawane@users.noreply.github.com>
2023-02-08 13:27:01 +0530

    Add files via upload

---
+++ OutKeyword/OutKeyword/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OutKeyword
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int result;
+            SumOfTwoNumber(2, 2, out result);
+            Console.WriteLine(result);
+
+            Console.ReadKey();
+        }
+
+        static void SumOfTwoNumber(int no1, int no2, out int result)
+        {
+            result = no1 + no2;
+        }
+    }
+}
